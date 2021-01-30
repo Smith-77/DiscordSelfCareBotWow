8dae119772091147de4b4b6e8311a6980f102da8
tim23 <tim23@vt.edu>
2021-01-30 17:06:22 -0500

    Updating the env

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-import discord
+# bot.py
 import os
+import random
+from dotenv import load_dotenv
 
-client = discord.Client()
+# 1
+from discord.ext import commands
 
-@client.event
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# 2
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
+@bot.command(name='exercise', hep='Suggests an activity that will make you get up and move!')
+async def offer_exercise(ctx):
+    exercises=['walk','run','stretch','yoga']
+    response = random.choice(exercises)
+    await ctx.send(response)
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    elif 'depression' in message.content.lower():
+        response = "No"
+        await message.channel.send(response)
+    await bot.process_commands(message)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.getenv('TOKEN'))
+bot.run(TOKEN)
