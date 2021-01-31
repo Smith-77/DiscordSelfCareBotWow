a23514f8feb119f4d1692772db2956a3c1f8f323
tim23 <tim23@vt.edu>
2021-01-31 10:54:20 -0500

    Adding gifs

--- privateMethods/negative_patterns.py
+++ privateMethods/negative_patterns.py
@@ -1,21 +1,28 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
+import random
 
-stress_keywords = ['stress','anxiety','anxious','stressed','tired', 'scared', 'afraid', 'fear']
+stress_keywords = ['stress','anxiety','anxious','stressed','tired', 'scared', 'afraid', 'fear','need help','need support']
 depression_keywords = ['end my life','depression','anxiety','anxious','depressed','I want to die','self harm','cut myself',"I don't wnat to live",'sad','mental health','suicide','kill myself']
-school_keywords = ['school','homework','test','quiz','paper','anxious','stressed','tired', 'scared', 'afraid', 'fear']
+school_keywords = ['school','homework','test','quiz','paper']
+
+gifs = ['https://media.discordapp.net/attachments/803257943645880323/805302433390788648/giphy_5.gif',
+    'https://cdn.discordapp.com/attachments/805132413146759170/805461686479093780/giphy_24.gif',
+    'https://cdn.discordapp.com/attachments/805132413146759170/805461542220988456/giphy_4.gif',
+    'https://cdn.discordapp.com/attachments/805132413146759170/805461539737829376/giphy_6.gif']
 
 # This message formulates and returns a message to a user message containing a depression pattern
 def get_depression_response():
     intro = "Hey! Are you okay? If you're struggling with anything, you're not alone and help is available. Here are some resources:"
     resource1 = "  -  National Suicide Prevention Lifeline: call 800-273-8255 or visit https://suicidepreventionlifeline.org/"
     resource2 = "  -  If you want to talk to people, check out this Discord: https://discord.gg/9g98gZ9H" 
-    resource3 = "  -  Please remember you are always loved: https://media.discordapp.net/attachments/803257943645880323/805302433390788648/giphy_5.gif"
+    resource3 = "  -  Please remember you are always loved: "
+    resource3 = resource3 + random.choice(gifs)
     return "{intro}\n\n{resource1}\n{resource2}\n{resource3}".format(intro = intro, resource1 = resource1, resource2 = resource2, resource3 = resource3)
 
 # This method determines if a message could contain a negative pattern of depression or anxiety
 # It uses a combination of keywords and sentiment analysis
 def contains_depression_traces(message_content):
-    depression_keywords = ['hate myself','end my life','depression','anxiety','anxious','depressed','I want to die','self harm','cut myself',"I don't wnat to live",'sad','mental health','suicide','kill myself']
+    depression_keywords = ['hate myself','my life','end my life','depression','anxiety','anxious','depressed','I want to die','self harm','cut myself',"I don't wnat to live",'sad','mental health','suicide','kill myself']
     sentiment_analyzer = SentimentIntensityAnalyzer()
     sentiment_dict = sentiment_analyzer.polarity_scores(message_content)
     negative_sentiment = sentiment_dict['neg']
@@ -28,14 +35,16 @@ def contains_depression_traces(message_content):
     return False
 
 def get_stress_response():
-    intro = "Hey! Are you okay? You sound stressed. What's the problem? School, work, relationships?"
+    intro = "Hey! Are you okay? You sound stressed. What's the problem? School, work, relationships? "
+    intro = intro + random.choice(gifs)
+    return intro
 
 def contains_stress_traces(message_content):
     sentiment_analyzer = SentimentIntensityAnalyzer()
     sentiment_dict = sentiment_analyzer.polarity_scores(message_content)
     negative_sentiment = sentiment_dict['neg']
     positive_sentiment = sentiment_dict['pos']
-    message_is_stressed = negative_sentiment > .85 or (negative_sentiment > .6 and positive_sentiment < .4)
+    message_is_stressed = negative_sentiment > .75 or (negative_sentiment > .6 and positive_sentiment < .4)
     for word in stress_keywords:
         if word in message_content :
             return message_is_stressed
